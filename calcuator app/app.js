09bbeb04c786f494ae85ceb71d8058c878aac1cc
FAHAD-ZAIB-Hanif <fahadzaib0011@gmail.com>
2024-05-13 22:44:55 +0500

    Hi, i create a simple calculator using Html and Js.

---
+++ calcuator app/app.js
@@ -0,0 +1,13 @@
+function add(num) {
+    document.getElementById('ipt').value = num
+}
+function add(num) {
+    document.getElementById('ipt').value += num
+}
+function eq(){
+    var a = document.getElementById('ipt')
+    a.value=eval(a.value);
+}
+function clr() {
+    document.getElementById('ipt').value = ''
+}
